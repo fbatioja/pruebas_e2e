720c12a9e88cfd8f24a024e46bb1a7163ec7eff8
Fabian Batioja <f.batioja@uniandes.edu.co>
2021-05-08 23:01:45 -0500

    Parametrizacion kraken

--- kraken/features/web/step_definitions/web_steps.rb
+++ kraken/features/web/step_definitions/web_steps.rb
@@ -2,10 +2,13 @@ if ENV["ADB_DEVICE_ARG"].nil?
   require 'kraken-mobile/steps/web/kraken_steps'
 
   Then(/^I login in ghost as admin$/) do
-    @driver.navigate.to 'http://localhost:2370/ghost/'
+    $email = 'admin@test.com'
+    $password = '1a2B3c4D,F'
+    $url = 'http://localhost:2370/ghost/'
+    @driver.navigate.to $url
       sleep 2
-    @driver.find_element(:css, 'input[name="identification"]').send_keys('admin@test.com')
-    @driver.find_element(:css, 'input[name="password"]').send_keys('1a2B3c4D,F')
+    @driver.find_element(:css, 'input[name="identification"]').send_keys($email)
+    @driver.find_element(:css, 'input[name="password"]').send_keys($password)
     @driver.find_element(:css, '#login button[type="submit"]').click
       sleep 2
   end
